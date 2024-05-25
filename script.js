a17cf6f5f18ea09d9dd5565673e29e37a2f36810
randomSmarts <aarshsmittal@gmail.com>
2024-05-24 20:29:45 -0700

    homepage

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to handle the "Select Playlist" button click
+function selectPlaylist() {
+    alert('Select Playlist button clicked');
+    // Add your playlist selection logic here
+}
+
+// Function to handle the "Reverse" button click
+function reverseAction() {
+    alert('Reverse button clicked');
+    // Add your reverse action logic here
+}
